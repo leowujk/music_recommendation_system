ea159d64b96559ce48cf0cc50a25b58823bf8840
leowujk <110680715+leowujk@users.noreply.github.com>
2024-06-03 17:56:47 +0800

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,25 +6,21 @@ import { faHome, faInfoCircle, faCogs, faEnvelope } from '@fortawesome/free-soli
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 
+
 // objects
 const Sidebar = () => {
     // return
     return (
         <div className="flex h-screen">
           <div className="w-64 bg-gray-800 text-white flex flex-col">
-            <div className="p-4">
-              <h1 className="text-lg font-bold">My Sidebar</h1>
-            </div>
             <div className="flex flex-col p-4">
+              <FontAwesomeIcon icon={faBars} className="mr-2" size="sm" />
                 {/* turn a link into div */}
               <Link href="/">
                 <div className="p-2 hover:bg-gray-700 rounded">Home</div>
               </Link>
               <Link href="/Explore">
-                <div className="p-2 hover:bg-gray-700 rounded">
-                  <FontAwesomeIcon icon={faBars} className="mr-2" />
-                  Explore
-                </div>
+                <div className="p-2 hover:bg-gray-700 rounded">Explore</div>
               </Link>
             </div>
           </div>
