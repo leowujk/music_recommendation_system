7c06d4540adcffa09fff873561b155a62f9c2def
leowujk <110680715+leowujk@users.noreply.github.com>
2024-06-17 18:08:17 +0800

    稍微調整了下song-inform的page.tsx

--- app/song-inform/[slug]/page.tsx
+++ app/song-inform/[slug]/page.tsx
@@ -1,18 +1,22 @@
 "use client"
 import React from 'react';
 
-const SongDetailPage: React.FC< { params: { slug: string } } >= ({ params }) => {
-
+const SongDetailPage: React.FC<{ params: { slug: string } }> = ({ params }) => {
     return (
-        <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white transform transition duration-300 hover:scale-105">
-            <div className="px-6 pb-4">
-                <div className="font-bold text-2xl mb-3 text-orange-800">{params.slug}</div>
-                <p className="text-lime-950 text-base mb-4">{params ? `${params} description` : ''}</p>
+        <div className="mt-10 ml-10 p-8 w-full max-w-4xl rounded-lg overflow-hidden shadow-lg bg-white transform transition duration-300 hover:scale-105 flex items-center">
+            <div className="px-6 pb-4 flex-1">
+                <p className="text-lime-950 text-base mb-4">{params ? `${params.slug} description` : ''}</p>
             </div>
-            <div className="px-6 pb-4">
-                <div
-                    className="inline-block bg-blue-500 hover:bg-blue-900 text-gray-100 font-bold px-4 py-2 rounded-full transition duration-300"
-                >
+            <div className="px-6 pb-4 flex-1">
+                <div className="font-bold text-2xl mb-3 text-orange-800">{params.slug}</div>
+                <br />
+                <div className="flex space-x-4">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        試聽
+                    </button>
+                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                        收藏
+                    </button>
                 </div>
             </div>
         </div>
